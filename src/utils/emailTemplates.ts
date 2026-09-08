d22db9bf94300be495633bb08ff20a53a52d0e56
Sheliss <sheliss.kai@gmail.com>
2026-09-08 13:25:48 +0300

    fix: repair malformed html href syntax in email verification template

--- src/utils/emailTemplates.ts
+++ src/utils/emailTemplates.ts
@@ -31,7 +31,7 @@ export const emailText = (verificationLink: string) => {
                               <table border="0" cellpadding="0" cellspacing="0" style="margin: 30px 0;">
                                   <tr>
                                       <td align="center" bgcolor="#000000" style="border-radius: 6px;">
-                                          <a href="{${verificationLink}}" target="_blank" style="font-size: 16px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; padding: 12px 24px; border-radius: 6px; border: 1px solid #000000; display: inline-block; font-weight: bold;">Verify Email Address</a>
+                                          <a href="${verificationLink}" target="_blank" style="font-size: 16px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; padding: 12px 24px; border-radius: 6px; border: 1px solid #000000; display: inline-block; font-weight: bold;">Verify Email Address</a>
                                       </td>
                                   </tr>
                               </table>
